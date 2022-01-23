a055c427ac037a221b766bcc22a6af5d1ab53c78
Farukh Saleem <fsaldev1@gmail.com>
2022-01-23 06:27:38 -0800

    snapshot tests

--- tests/pages/signUp/secure.test.tsx
+++ tests/pages/signUp/secure.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import { shallow, mount } from 'enzyme';
-import Create from '../../../pages/signup/create';
+import Secure from '../../../pages/signup/secure';
 import wrapper from '../../../store'
 
 it('should render correctly with no props', () => {
-  const CreateWrapper = wrapper.withRedux(Create)
-  const component = mount(<CreateWrapper/>)
+  const SecureWrapper = wrapper.withRedux(Secure)
+  const component = mount(<SecureWrapper/>)
   
   
   expect(component).toMatchSnapshot();
